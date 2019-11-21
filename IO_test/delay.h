3f39014a9c1342270582c70474c7f17eada9b332
wyx227 <wyx227@hotmail.com>
2019-11-21 15:19:23 +0100

    21.11.19 Debugging Programm
    + Debugging Interface
    + LCD-Unterstützung
    + LED-Ausgabe
    + Linearisierung

---
+++ IO_test/delay.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <wiringPi.h>
+
+void delay_no_itr(unsigned long duration)  //Aufruf der Delay-Funktion ohne Interrupt, um Effizienz zu erhöhen.
+{
+	unsigned long current = millis();
+	if (millis() - current == duration) {
+		current = millis();
+	}
+}
---
+++ IO_test/meldung.h
@@ -0,0 +1,7 @@
+#pragma once
+char *fatal_fehler = "Fataler ERROR!\n";
+char *datei_fehler = "Datenfehler!\n";
+char *restart_required = "Restart benötigt\n";
+char *play_pressed = "PLAY gedrückt!\n";
+char *vor_pressed = "VOR gedrückt!\n";
+char *rueck_pressed = "RUECK gedrückt!\n";
